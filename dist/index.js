f55d96050ab86124ccf93c961946da2806de8538
LexLuthor <140027760+LexLuthorDev@users.noreply.github.com>
2023-07-20 02:03:23 -0300

    ok

--- dist/index.js
+++ dist/index.js
@@ -1,55 +1,62 @@
-(function (global, factory) {
-  // Verifica se estamos no ambiente do Node.js (CommonJS)
-  if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+
+// sua-biblioteca.js
+(function (root, factory) {
+  if (typeof module === 'object' && typeof module.exports === 'object') {
+    // Node.js
     module.exports = factory();
   } else {
-    // Estamos no navegador ou ambiente AMD
-    global.formatarMoeda = factory();
+    // Navegador
+    root.suaBiblioteca = factory();
   }
-})(typeof window !== 'undefined' ? window : this, function () {
-  function formatarMoeda(valor, moeda) {
-    const formatosMoeda = {
-      'BRL': {
-        simbolo: 'R$',
-        separadorDecimal: ',',
-        separadorMilhares: '.',
-      },
-      'USD': {
-        simbolo: 'US$',
-        separadorDecimal: '.',
-        separadorMilhares: ',',
-      },
-      'EUR': {
-        simbolo: '€',
-        separadorDecimal: ',',
-        separadorMilhares: '.',
-      },
-      // Adicione outros formatos de moeda aqui, se necessário
-    };
+})(typeof self !== 'undefined' ? self : this, function () {
+  // Defina sua biblioteca aqui
 
-    if (typeof valor !== 'number') {
-      throw new Error('O valor fornecido não é um número.');
+  var suaBiblioteca = {
+    // Implemente suas funções e métodos aqui
+    formatarMoeda: function (valor, moeda) {
+      const formatosMoeda = {
+        'BRL': {
+          simbolo: 'R$',
+          separadorDecimal: ',',
+          separadorMilhares: '.',
+        },
+        'USD': {
+          simbolo: 'US$',
+          separadorDecimal: '.',
+          separadorMilhares: ',',
+        },
+        'EUR': {
+          simbolo: '€',
+          separadorDecimal: ',',
+          separadorMilhares: '.',
+        },
+        // Adicione outros formatos de moeda aqui, se necessário
+      };
+  
+      if (typeof valor !== 'number') {
+        throw new Error('O valor fornecido não é um número.');
+      }
+  
+      if (!formatosMoeda[moeda]) {
+        throw new Error('Moeda não suportada.');
+      }
+  
+      const formato = formatosMoeda[moeda];
+      const valorFormatado = valor.toLocaleString('pt-BR', {
+        style: 'currency',
+        currency: moeda,
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+      });
+  
+      // Substitui o símbolo e os separadores pelos definidos no objeto formatosMoeda
+      return valorFormatado
+        .replace(formato.simbolo, `${formato.simbolo} `)
+        .replace(/\./g, formato.separadorMilhares)
+        .replace(/,/g, formato.separadorDecimal);
     }
+  };
 
-    if (!formatosMoeda[moeda]) {
-      throw new Error('Moeda não suportada.');
-    }
-
-    const formato = formatosMoeda[moeda];
-    const valorFormatado = valor.toLocaleString('pt-BR', {
-      style: 'currency',
-      currency: moeda,
-      minimumFractionDigits: 2,
-      maximumFractionDigits: 2,
-    });
-
-    // Substitui o símbolo e os separadores pelos definidos no objeto formatosMoeda
-    return valorFormatado
-      .replace(formato.simbolo, `${formato.simbolo} `)
-      .replace(/\./g, formato.separadorMilhares)
-      .replace(/,/g, formato.separadorDecimal);
-  }
-
-  // Retorna a função formatarMoeda para ser disponibilizada no ambiente correto
-  return formatarMoeda;
+  return suaBiblioteca;
 });
+
