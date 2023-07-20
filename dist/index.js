fb145565a0d06bbf678337c8a926fb48acac4cea
LexLuthor <140027760+LexLuthorDev@users.noreply.github.com>
2023-07-20 01:45:57 -0300

    ok

--- dist/index.js
+++ dist/index.js
@@ -1,56 +1,63 @@
-function formatarMoeda(valor, moeda) {
-  const formatosMoeda = {
-    'BRL': {
-      simbolo: 'R$',
-      separadorDecimal: ',',
-      separadorMilhares: '.',
-    },
-    'USD': {
-      simbolo: 'US$',
-      separadorDecimal: '.',
-      separadorMilhares: ',',
-    },
-    'EUR': {
-      simbolo: '€',
-      separadorDecimal: ',',
-      separadorMilhares: '.',
-    },
-    // Adicione outros formatos de moeda aqui, se necessário
-  };
-
-  if (typeof valor !== 'number') {
-    throw new Error('O valor fornecido não é um número.');
+(function (global, factory) {
+  // Verifica o tipo de ambiente em que estamos (Node.js ou navegador)
+  if (typeof module === 'object' && typeof module.exports === 'object') {
+    // Estamos no Node.js
+    module.exports = factory();
+  } else if (typeof define === 'function' && define.amd) {
+    // Caso de uso AMD (por exemplo, RequireJS)
+    define([], factory);
+  } else {
+    // Estamos no navegador, expõe globalmente
+    global.formatarMoeda = factory();
   }
+})(this, function () {
+  function formatarMoeda(valor, moeda) {
+    const formatosMoeda = {
+      'BRL': {
+        simbolo: 'R$',
+        separadorDecimal: ',',
+        separadorMilhares: '.',
+      },
+      'USD': {
+        simbolo: 'US$',
+        separadorDecimal: '.',
+        separadorMilhares: ',',
+      },
+      'EUR': {
+        simbolo: '€',
+        separadorDecimal: ',',
+        separadorMilhares: '.',
+      },
+      // Adicione outros formatos de moeda aqui, se necessário
+    };
 
-  if (!formatosMoeda[moeda]) {
-    throw new Error('Moeda não suportada.');
+    if (typeof valor !== 'number') {
+      throw new Error('O valor fornecido não é um número.');
+    }
+
+    if (!formatosMoeda[moeda]) {
+      throw new Error('Moeda não suportada.');
+    }
+
+    const formato = formatosMoeda[moeda];
+    const valorFormatado = valor.toLocaleString('pt-BR', {
+      style: 'currency',
+      currency: moeda,
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    });
+
+    // Substitui o símbolo e os separadores pelos definidos no objeto formatosMoeda
+    return valorFormatado
+      .replace(formato.simbolo, `${formato.simbolo} `)
+      .replace(/\./g, formato.separadorMilhares)
+      .replace(/,/g, formato.separadorDecimal);
   }
+  // Exemplos de uso da função após importação
+  console.log(formatarMoeda(100, 'BRL')); // Output: R$ 100,00
+  console.log(formatarMoeda(50, 'USD'));  // Output: US$ 50,00
+  console.log(formatarMoeda(30, 'EUR'));  // Output: € 30,00
 
-  const formato = formatosMoeda[moeda];
-  const valorFormatado = valor.toLocaleString('pt-BR', {
-    style: 'currency',
-    currency: moeda,
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2,
-  });
-
-  // Substitui o símbolo e os separadores pelos definidos no objeto formatosMoeda
-  return valorFormatado
-    .replace(formato.simbolo, `${formato.simbolo} `)
-    .replace(/\./g, formato.separadorMilhares)
-    .replace(/,/g, formato.separadorDecimal);
-}
-
-// Exportar a função para uso em outros arquivos (no Node.js) ou para o navegador
-if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
-  // Estamos no Node.js
-  module.exports = formatarMoeda;
-} else if (typeof window !== 'undefined') {
-  // Estamos no navegador
-  window.formatarMoeda = formatarMoeda;
-}
-
-// Exemplos de uso (mantidos para referência):
-console.log(formatarMoeda(100, 'BRL')); // Output: R$ 100,00
-console.log(formatarMoeda(50, 'USD'));  // Output: US$ 50,00
-console.log(formatarMoeda(30, 'EUR'));  // Output: € 30,00
+  // Retorna a função formatarMoeda para ser disponibilizada no ambiente correto
+  return formatarMoeda;
+});
